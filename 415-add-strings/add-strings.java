8c42bb61d21f28e1c47fd426dfbf1701211d7718
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-03-14 21:45:14 +0530

    Time: 2 ms (98.69%) | Memory: 44.1 MB (35.42%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,43 +1,21 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        if(num1.length() < num2.length()){
-            String t = num1;
-            num1 = num2;
-            num2 = t;
-        }
-
-        int s1 = num1.length()-1;
-        int s2 = num2.length()-1;
+        
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length()-1;
+        int j = num2.length()-1;
         int carry = 0;
-        String ans = "";
 
-        while(s2 >= 0){
-            char c1 = num1.charAt(s1);
-            char c2 = num2.charAt(s2);
+        while(i >= 0 || j >= 0 || carry > 0){
+            int d1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int d2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
 
-            int temp1 = (c1 - '0') + (c2 - '0') + carry;
-            int temp2 = temp1 % 10; 
-
-            ans = String.valueOf(temp2) + ans;
-            carry = temp1 / 10;
-            s1--;
-            s2--;
+            int sum = d1 + d2 + carry;
+            carry = sum / 10;
+            sb.append(sum % 10);
+            i--;
+            j--;
         }
-        if(carry == 0) return num1.substring(0, s1+1) + ans;
-
-        while(s1 >= 0){
-            if(carry > 0){
-                int c = num1.charAt(s1) - '0';
-                int t1 = (c + carry) % 10;
-                ans = String.valueOf(t1) + ans;
-                carry = (c + carry) / 10;
-            }
-            else{
-                ans = num1.substring(0, s1+1) + ans;
-                return carry == 0 ? ans : "1" + ans;
-            } 
-            s1--;
-        }
-        return carry == 0 ? ans : "1" + ans;
+        return sb.reverse().toString();
     }
 }
