160417e30c2ccbdfd92a8b2c98a1ae0d0c422367
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-03-14 21:49:45 +0530

    Time: 2 ms (98.69%) | Memory: 43.8 MB (75.87%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -5,16 +5,22 @@ class Solution {
         int i = num1.length()-1;
         int j = num2.length()-1;
         int carry = 0;
+        
 
         while(i >= 0 || j >= 0 || carry > 0){
-            int d1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
-            int d2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+            int d1 = 0,d2 = 0;
+            if(i >= 0){
+                d1 = num1.charAt(i) - '0';
+                i--;
+            }
+            if(j >= 0){
+                d2 = num2.charAt(j) - '0';
+                j--;
+            }
 
             int sum = d1 + d2 + carry;
             carry = sum / 10;
             sb.append(sum % 10);
-            i--;
-            j--;
         }
         return sb.reverse().toString();
     }
