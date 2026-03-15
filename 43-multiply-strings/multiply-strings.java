dadaef386e27c876ff61aa5fa66d83ef1c16a077
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-03-15 21:12:35 +0530

    Time: 3 ms (84.11%) | Memory: 43.7 MB (66.93%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,34 +1,25 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int n1 = num1.length();
-        int n2 = num2.length();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0;i < n1+n2;i++) sb.append('0');
+        int n1 = num1.length(), n2 = num2.length();
+        int arr[] = new int[n1 + n2];
 
         for(int i = n1-1;i >= 0;i--){
             for(int j = n2-1;j >= 0;j--){
-                int c1 = num1.charAt(i) - '0';
-                int c2 = num2.charAt(j) - '0';
+                int mul = (num1.charAt(i)-'0') * (num2.charAt(j) - '0');
+                int p1 = i+j+1;
+                int p2 = i+j;
 
-                int prod = c1 * c2;
-                int rem = prod % 10;
-                int c = prod / 10;
-
-                helper(sb, rem, i+j+1);
-                helper(sb, c, i+j);
+                int sum = arr[p1] + mul;
+                arr[p1] = sum % 10;
+                arr[p2] += sum / 10;
             }
         }
-        while(sb.length() > 1 && sb.charAt(0) == '0') sb.deleteCharAt(0);
 
-        return sb.toString();
-    }
-
-    public void helper(StringBuilder sb, int val, int pos){
-      
-        int sum = sb.charAt(pos)-'0' + val;
-        sb.setCharAt(pos, (char)(sum%10 + '0'));
-        if(sum > 9){
-            helper(sb, sum / 10, pos-1);
+        StringBuilder sb = new StringBuilder();
+        for(int v : arr){
+            if(!(sb.length() == 0 && v == 0)) sb.append(v);
         }
+
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
