7ad6030b27a5dfcdc848d646997288aa4e4f868f
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-04-05 13:45:45 +0530

    Time: 7 ms (50.00%) | Memory: 114.1 MB (42.51%) - LeetSync

--- 4149-minimum-increase-to-maximize-special-indices/minimum-increase-to-maximize-special-indices.java
+++ 4149-minimum-increase-to-maximize-special-indices/minimum-increase-to-maximize-special-indices.java
@@ -1,34 +1,28 @@
 class Solution {
     public long minIncrease(int[] nums) {
-        int n= nums.length;
-        long dp[][] = new long[n][2];
-        for(long[] i : dp) Arrays.fill(i, -1);
-        long arr[] = f(nums.length-2, nums, dp);
-        return arr[1];
-    }
+        int n = nums.length;
 
-    public static long[] f(int i, int nums[], long dp[][]){
-        if(i == 1){
-            int temp =Math.max(0, Math.max(nums[i-1], nums[i+1])+1 - nums[i]);
-            return new long[]{1, temp};
-        }
-        if(i == 0){
-            return new long[]{0, 0};
-        }
-        
-        if(dp[i][0] != -1) return dp[i];
+        long[] prev2 = {0, 0};
+        int t = Math.max(0, Math.max(nums[0], nums[2])+1 - nums[1]);
+        long[] prev1 = {1, t};
 
-        long not_pick[] = f(i-1, nums, dp);
-        
-        int temp = Math.max(0, Math.max(nums[i-1], nums[i+1])+1 - nums[i]);
-        long prevpick[] = f(i-2, nums, dp);
-        long pick[] =new long[]{prevpick[0]+1, prevpick[1]+temp};
+        for(int i = 2;i < n-1;i++){
+            long curr[] = new long[2];
+            int temp = Math.max(0, Math.max(nums[i-1], nums[i+1])+1 - nums[i]);
 
-        if(pick[0] == not_pick[0]){
-            dp[i] = pick[1] <= not_pick[1] ? pick : not_pick;
-        } else {
-            dp[i] = pick[0] > not_pick[0] ? pick : not_pick; 
+            if(prev2[0]+1 == prev1[0]){
+                curr[0] = prev1[0];
+                curr[1] = Math.min(prev2[1]+temp, prev1[1]);
+            } else if(prev2[0]+1 > prev1[0]){
+                curr[0] = prev2[0]+1;
+                curr[1] = prev2[1] + temp;
+            } else {
+                curr = prev1;
+            }
+
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[i];
+        return prev1[1];
     }
 }
