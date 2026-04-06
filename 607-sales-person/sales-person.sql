5340386003bc4c2b1d1b4c81b2d5c7629aeb92dd
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-04-06 12:27:38 +0530

    Time: 1396 ms (42.01%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 SELECT name
-FROM SalesPerson 
-WHERE name NOT IN (SELECT s.name
-FROM SalesPerson s
-JOIN Orders o
-ON s.sales_id = o.sales_id
-WHERE o.com_id = (SELECT com_id FROM Company WHERE name = "RED"))
+FROM salesperson s
+WHERE NOT EXISTS (SELECT 1
+                  FROM company c
+                  JOIN orders o
+                  ON o.com_id = c.com_id
+                  WHERE s.sales_id = o.sales_id
+                  AND c.name = "RED")
