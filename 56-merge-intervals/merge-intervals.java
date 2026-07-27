4689c3b92c75135355ac2a5d89e1685245321f7a
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-07-27 20:56:07 +0530

    Time: 10 ms (21.84%) | Memory: 48.8 MB (92.69%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,30 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
-
-        Arrays.sort(intervals, (a, b) -> {
-            if(a[0] == b[0]) return a[1] - b[1];
-            return a[0] - b[0];
-        });
-
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         ArrayList<List<Integer>> list = new ArrayList<>();
-        int i = 0;
+        int i = 1;
         int one = intervals[0][0];
         int two = intervals[0][1];
 
         while(i < n){
+           if(two >= intervals[i][0]){
+            one = Math.min(one, intervals[i][0]);
+            two = Math.max(two, intervals[i][1]);
+           }else {
+            list.add(new ArrayList<>(Arrays.asList(one, two)));
             one = intervals[i][0];
             two = intervals[i][1];
-
-            while(i+1 < n && two >= intervals[i+1][0]){
-                two = Math.max(two, intervals[i+1][1]);
-                i++;
-            }
-
-            list.add(new ArrayList<>(Arrays.asList(one, two)));
-            i++;
+           }
+           i++;
         }
-
+       
+        list.add(new ArrayList<>(Arrays.asList(one, two)));
         int arr[][] = new int[list.size()][2];
 
         for(i = 0;i < list.size();i++){
