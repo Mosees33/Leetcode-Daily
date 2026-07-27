18f1461ce148fb29d8d791983c6d56c3ab3d977c
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-07-27 20:17:18 +0530

    Time: 5 ms (61.63%) | Memory: 44.2 MB (90.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-
-        char sa[] = s.toCharArray();
-        char ta[] = t.toCharArray();
-
-        Arrays.sort(sa);
-        Arrays.sort(ta);
+        int arr[] = new int[26];
 
         for(int i = 0;i < s.length();i++){
-            if(sa[i] != ta[i]) return false;
+            arr[s.charAt(i)-'a'] += 1;
+            arr[t.charAt(i)-'a'] -= 1;
+        }
+        for(int i : arr){
+            if(i != 0) return false;
         }
 
         return true;
