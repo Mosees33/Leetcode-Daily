92de8b575279e8416705b10394460b945ae899e8
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-07-29 19:02:52 +0530

    Time: 2 ms (96.39%) | Memory: 45.8 MB (33.92%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        f(0, target, candidates, list, ans);
+        f(0, candidates, target, ans, new ArrayList<>());
         return ans;
     }
 
-    public void f(int i, int target, int candidates[], List<Integer> list, List<List<Integer>> ans){
-        if(i == candidates.length || target < 0) return;
-        if(target == 0){
-            ans.add(new ArrayList<>(list));
+    public void f(int i, int nums[], int target, List<List<Integer>> ans, List<Integer> list){
+        if(i == nums.length){
+            if(target == 0) ans.add(new ArrayList<>(list));
             return;
         }
-        
-        f(i+1, target, candidates, list, ans);
-        list.add(candidates[i]);
-        f(i, target-candidates[i], candidates, list, ans);
-        target += candidates[i];
-        list.remove(list.size()-1);
-        
 
+        if(nums[i] <= target){
+            list.add(nums[i]);
+            f(i, nums, target-nums[i], ans, list);
+            list.remove(list.size()-1);
+        }
+        f(i+1, nums, target, ans, list);
     }
 }
