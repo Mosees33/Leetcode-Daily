04273692886a38df7d431ded3195c6f9113af0da
Moseeswaran C <148618498+Mosees33@users.noreply.github.com>
2026-08-01 18:33:08 +0530

    Time: 5 ms (99.82%) | Memory: 43.1 MB (77.64%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,24 +1,14 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        Arrays.sort(coins);
+        int dp[] = new int[amount+1];
+        dp[0] = 1;
 
-        int dp[][] = new int[coins.length][amount+1];
-        for(int row[] : dp) Arrays.fill(row, -1);
-
-        return f(coins.length-1, amount, coins, dp);
-    }
-
-    public int f(int n, int amount, int coins[], int dp[][]){
-        if(n == 0){
-            return dp[n][amount] = amount % coins[0] == 0 ? 1 : 0;
+        for(int coin : coins){
+            for(int i = coin;i <= amount;i++){
+                dp[i] += dp[i-coin];
+            }
         }
-
-        if(dp[n][amount] != -1) return dp[n][amount];
-
-        int pick = 0;
-        if(amount >= coins[n]) pick = f(n, amount-coins[n], coins, dp);
-        int n_pick = f(n-1, amount, coins, dp);
-
-        return dp[n][amount] = pick + n_pick;
+        
+        return dp[amount];
     }
 }
